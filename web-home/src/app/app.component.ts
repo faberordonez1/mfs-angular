e37cc9fc54e9f1dbff24fbae3fab6aa02ebf04e9
Faber OrdoñeZ <faberordonez1@gmail.com>
2025-07-16 22:48:47 -0500

    fix recargas

--- web-home/src/app/app.component.ts
+++ web-home/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-home';
+
+  crear() {
+    // Lógica para crear un nuevo elemento
+    console.log('Crear nuevo elemento');
+    var nuevoElemento = {
+      id: Date.now(),
+      nombre: 'Nuevo Elemento'
+    };
+    console.log(nuevoElemento);
+  }
 }
