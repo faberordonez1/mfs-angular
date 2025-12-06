90fcac5fb339570835dc06d581ac706fc47b9af8
faberordonez1@gmail.com <faberordonez1@gmail.com>
2025-12-05 21:08:46 -0500

    Reset

--- web-apuestas/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  homeUrl: 'https://home.production.com',
-  recargasUrl: 'https://recargas.production.com',
-  apuestasUrl: 'https://apuestas.production.com'
-};
