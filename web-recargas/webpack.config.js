9c7e2ae8b7a00e147beb3620ef895ec21a36a338
faberordonez1@gmail.com <faberordonez1@gmail.com>
2025-12-08 20:54:08 -0500

    web-recargas Se implementan cambios microfronted con module federation

---
+++ web-recargas/webpack.config.js
@@ -0,0 +1,19 @@
+const { withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+const config = withModuleFederationPlugin({
+  name: 'web_recargas',
+  exposes: {
+    './Routes': './src/app/app.routes.ts',
+  },
+  filename: 'remoteEntry.js',
+  shared: {
+    '@angular/core': { singleton: true, strictVersion: true },
+    '@angular/common': { singleton: true, strictVersion: true },
+    '@angular/router': { singleton: true, strictVersion: true },
+  },
+});
+
+config.output.publicPath = process.env.PUBLIC_PATH || 'auto';
+config.output.uniqueName = 'web_recargas';
+
+module.exports = config;
