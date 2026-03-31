3b200e5d2dc5482ee144eaf8b8acf0f8645c5ac0
faberordonez1@gmail.com <faberordonez1@gmail.com>
2026-03-30 22:21:27 -0500

    Routing lazy + environment

--- web-home/src/app/app-routing.module.ts
+++ web-home/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MnComponent } from './mn/mn.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
+import { environment } from '../environments/environment';
 
 const routes: Routes = [
   {
@@ -16,7 +17,7 @@ const routes: Routes = [
     path: 'recargas',
     loadChildren: () =>
       loadRemoteModule({
-        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        remoteEntry: `${environment.recargasUrl}remoteEntry.js`,
         remoteName: 'web_recargas',
         exposedModule: './RecargasModule'
       }).then((m) => m.RecargasModule)
@@ -25,7 +26,7 @@ const routes: Routes = [
     path: 'apuestas',
     loadChildren: () =>
       loadRemoteModule({
-        remoteEntry: 'http://localhost:4202/remoteEntry.js',
+        remoteEntry: `${environment.apuestasUrl}remoteEntry.js`,
         remoteName: 'web_apuestas',
         exposedModule: './ApuestasModule'
       }).then((m) => m.ApuestasModule)
